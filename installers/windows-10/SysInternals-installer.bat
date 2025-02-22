dae42fc5c24ac3618e320806f1eb4ca88f00f3af
hexwreaker <hexwreaker@pm.me>
2025-02-22 20:11:37 +0100

    update sysinternal installer windows 10

--- installers/windows-10/SysInternals-installer.bat
+++ installers/windows-10/SysInternals-installer.bat
@@ -2,5 +2,8 @@
 echo "Install SysInternals Tools"
 curl -OL https://download.sysinternals.com/files/SysinternalsSuite.zip
 echo "Extracting SysInternals"
+mkdir SysinternalsSuite
+cd SysinternalsSuite
+mv ../SysinternalsSuite.zip .
 tar -xf SysinternalsSuite.zip
 
