1fae89b58fbfc860094412bbb2c70f50fb0e0362
hexwreaker <hexwreaker@pm.me>
2025-03-01 18:42:56 +0100

    Update oleViewDotNet installer

--- installers/windows-10/OleViewDotNet-installer.bat
+++ installers/windows-10/OleViewDotNet-installer.bat
@@ -1,3 +1,13 @@
+echo "Install 7z archiver"
+curl -OL https://www.7-zip.org/a/7z2409-x64.exe
+7z2409-x64.exe
+
 # Install OleViewDotNet
-echo "Install OleViewDotNet : REQUIRE ADMIN RIGHTS !"
-powershell -Command "Install-Module -Name OleViewDotNet"
+echo "Install RPCView"
+curl -OL https://github.com/tyranid/oleviewdotnet/releases/download/v1.11/Release.7z
+echo "Extract OleViewDotNet"
+mkdir OleViewDotNet
+mv Release.7z OleViewDotNet\
+cd OleViewDotNet
+"C:\Program Files\7-Zip\7z.exe" x Release.7z
+
